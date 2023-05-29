d160af866d44e67b80e6048ef61f3dde240c89fd
Lucas Henrique Santos de Bairros <124683425+lucashsbifsc@users.noreply.github.com>
2023-05-29 08:55:08 -0300

    Enviado os arquivos do EDC3
    Adicionado os arquivos .mwb e .png do Diagrama Karts.
    Adicionado o arquivo .sql do Diagrama Karts (código).
    Estudo de Caso 3.

---
+++ ArquivoKarts.sql
@@ -0,0 +1,78 @@
+CREATE DATABASE IF NOT EXISTS diagrama_karts;
+
+-- -----------------------------------------------------
+-- Table `fornecedor`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `fornecedor` (
+  `cpnj` INT(14) NOT NULL,
+  `nome_empresa` VARCHAR(80) NOT NULL,
+  `cep` INT(8) NOT NULL,
+  `telefone` INT(13) NOT NULL,
+  PRIMARY KEY (`cpnj`)
+);
+
+-- -----------------------------------------------------
+-- Table `karts`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `karts` (
+  `id_kart` INT NOT NULL,
+  `cor` VARCHAR(45) NOT NULL,
+  `modelo` VARCHAR(45) NOT NULL,
+  `marca` VARCHAR(45) NOT NULL,
+  `ano` DATETIME(4) NOT NULL,
+  `quantidade` INT(3) NOT NULL,
+  `data_entrada` DATETIME(8) NOT NULL,
+  `fornecedor_cpnj` INT(14) NOT NULL,
+  PRIMARY KEY (`id_kart`),
+  CONSTRAINT `fk_karts_fornecedor` FOREIGN KEY (`fornecedor_cpnj`) REFERENCES `fornecedor` (`cpnj`)
+);
+
+-- -----------------------------------------------------
+-- Table `funcionarios`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `funcionarios` (
+  `matricula` INT(11) NOT NULL,
+  `cpf` INT(11) NOT NULL,
+  `nome_completo` VARCHAR(80) NOT NULL,
+  `data_nascimento` DATETIME(8) NOT NULL,
+  `cargo` VARCHAR(80) NOT NULL,
+  `senha` VARCHAR(80) NOT NULL,
+  PRIMARY KEY (`matricula`)
+);
+
+-- -----------------------------------------------------
+-- Table `clientes`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `clientes` (
+  `id_cliente` INT NOT NULL AUTO_INCREMENT,
+  `nome_completo` VARCHAR(80) NOT NULL,
+  `data_nascimento` DATETIME(8) NOT NULL,
+  `cnpj` INT(12) NULL,
+  `cpf` INT(11) NULL,
+  PRIMARY KEY (`id_cliente`)
+);
+
+-- -----------------------------------------------------
+-- Table `vendas`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `vendas` (
+  `id_vendas` INT NOT NULL,
+  `data_venda` DATETIME(8) NOT NULL,
+  `funcionarios_matricula` INT(11) NOT NULL,
+  `clientes_id_cliente` INT NOT NULL,
+  `valor_total` FLOAT NOT NULL,
+  PRIMARY KEY (`id_vendas`),
+  CONSTRAINT `fk_vendas_funcionarios1` FOREIGN KEY (`funcionarios_matricula`) REFERENCES `funcionarios` (`matricula`) CONSTRAINT `fk_vendas_clientes1` FOREIGN KEY (`clientes_id_cliente`) REFERENCES `clientes` (`id_cliente`)
+);
+
+-- -----------------------------------------------------
+-- Table `compras`
+-- -----------------------------------------------------
+CREATE Table IF NOT EXISTS `compras` (
+  `karts_id_kart` INT NOT NULL,
+  `vendas_id_vendas` INT NOT NULL,
+  `quantidade` INT(3) NOT NULL,
+  `data_compra` DATETIME(8) NOT NULL,
+  PRIMARY KEY (`karts_id_kart`, `vendas_id_vendas`),
+  CONSTRAINT `fk_karts_has_vendas_karts1` FOREIGN KEY (`karts_id_kart`) REFERENCES `karts` (`id_kart`) CONSTRAINT `fk_karts_has_vendas_vendas1` FOREIGN KEY (`vendas_id_vendas`) REFERENCES `vendas` (`id_vendas`)
+);
